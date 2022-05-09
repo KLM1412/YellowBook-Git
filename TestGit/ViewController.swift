da25642304bd5a13549d88227390f410696ba32c
klm1412 <1366047541@qq.com>
2022-05-09 16:00:21 +0800

    做了UI

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by kid on 2022/5/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("做了UI")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
